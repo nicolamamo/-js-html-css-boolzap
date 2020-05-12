f7d64e8d09763d41fccf9e6183e04f96f34c0ccc
nicolamamo <62040103+nicolamamo@users.noreply.github.com>
2020-05-12 08:25:17 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 
 //cliccando microfono utente visualizza il suo messaggio
-$('#microfono').click(function(){
-    alert("ciao");
+$("#invia").click(function(){
+ //alert('inizio')
+
+
+
+     var messaggio_utente = $('#mex').val()
+
+     /$('#dialogo').append( '<div id='mio'>' +' messaggio_utente' + '</div>')
+
+
+
 })
+//    var messaggio_utente = $('#mex').val()
+
+    //$('#dialogo').append( '<div id='mio'>' + messaggio_utente + '</div>')
