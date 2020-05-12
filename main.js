3f2ed971cdab6dd6d7d888ceeb075caf252996c7
nicolamamo <62040103+nicolamamo@users.noreply.github.com>
2020-05-12 19:13:12 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 
 //cliccando microfono utente visualizza il suo messaggio
-    $("#invia").click(function(){
+    $("#invia").click(invio)
 
 //creo testo utente
-     var messaggio_utente = $('#send_it').val();
-     console.log(messaggio_utente);
-    // $('.dialogo').append( messaggio_utente);
-//creo template nuvo messaggio e lo copip
-     var nuovo_messaggio = $('.template .messaggio' ).clone();
-//aggiungo testo
-     nuovo_messaggio.text(messaggio_utente);
-//aggiungo il nuvo messaggio
-     $('.dialogo').append(nuovo_messaggio)
 
-})
 //    var messaggio_utente = $('#mex').val()
 
     //$('#dialogo').append( '<div id='mio'>' + messaggio_utente + '</div>')
 
 
 // premendo invio il messaggio viene visualizzato
+function invio() {
+    var messaggio_utente = $('#send_it').val();
+    console.log(messaggio_utente);
+   // $('.dialogo').append( messaggio_utente);
+//creo template nuvo messaggio e lo copip
+    var nuovo_messaggio = $('.template .messaggio' ).clone();
+//aggiungo testo
+    nuovo_messaggio.text(messaggio_utente);
+
+     $('.dialogo').append(nuovo_messaggio);
+
+     $('send_it').keypress(function(e) {
+       if(e.which == 13) {
+         console.log("enter");
+         $(".dialogo").append(nuovo_messaggio);
+       }
+})
+}
