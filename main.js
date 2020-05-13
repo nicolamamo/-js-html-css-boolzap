b8397f060017de279639604c6394f77ff17cec22
nicolamamo <62040103+nicolamamo@users.noreply.github.com>
2020-05-13 16:37:35 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
+$('#invia').click(invia_messaggio);
 
-//cliccando microfono utente visualizza il suo messaggio
-    $("#invia").click(invio)
+$('#send_it').keypress(function(event){
+    if (event.wich == 13) {
+        invia_messaggio();
+    }
+})
+
+
+
+
+
+function invia_messaggio() {
 
 //creo testo utente
-
-//    var messaggio_utente = $('#mex').val()
-
-    //$('#dialogo').append( '<div id='mio'>' + messaggio_utente + '</div>')
-
-
-// premendo invio il messaggio viene visualizzato
-function invio() {
-    var messaggio_utente = $('#send_it').val();
-    console.log(messaggio_utente);
-   // $('.dialogo').append( messaggio_utente);
+     var messaggio_utente = $('#send_it').val();
+     console.log(messaggio_utente);
+    // $('.dialogo').append( messaggio_utente);
 //creo template nuvo messaggio e lo copip
-    var nuovo_messaggio = $('.template .messaggio' ).clone();
+     var nuovo_messaggio = $('.template .messaggio' ).clone();
 //aggiungo testo
-    nuovo_messaggio.text(messaggio_utente);
+     nuovo_messaggio.text(messaggio_utente);
+//aggiungo il nuvo messaggio
+     $('.dialogo').append(nuovo_messaggio)
 
-     $('.dialogo').append(nuovo_messaggio);
-
-     $('send_it').keypress(function(e) {
-       if(e.which == 13) {
-         console.log("enter");
-         $(".dialogo").append(nuovo_messaggio);
-       }
-})
+    //resetto l imput
+    $('#send_it').val('');
 }
